c0c63199c11ae382389e4b2336bf373bd4b70697
M0STAFA-A4F <mashraf45682@gmail.com>
2020-05-19 16:10:23 +0200

    teslm in main

--- src/montg3i/MONTG3I.java
+++ src/montg3i/MONTG3I.java
@@ -24,6 +24,7 @@ public class MONTG3I  {
         
         System.out.println("montg3i.MONTG3I.main()");
         System.out.println("a7laaaa mesaa 3aliik");
+        System.out.println("teslm ya 42ee2");
     } 
             
 }
